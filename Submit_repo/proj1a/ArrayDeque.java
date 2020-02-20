7366bb4b88cccc5a7e8bdd4290ba35944c41ca4a
Ching <yoyochingyu@gmail.com>
2020-02-20 16:50:11 +0800

    Proj1:ArrayList(everything except resizing)

--- Submit_repo/proj1a/ArrayDeque.java
+++ Submit_repo/proj1a/ArrayDeque.java
@@ -1,6 +1,7 @@
 /**
  * Invariants :
  * 1. You shouldn't link between array elements.
+ * 2. Remove first = remove index:size-1
  * */
 
 public class ArrayDeque{
@@ -9,12 +10,24 @@ public class ArrayDeque{
     public int nextFirst;
     public int nextLast;
 
+
     /** Constructor:empty array */
     public ArrayDeque(){
         items = new int [8];
         size = 0;
         nextFirst = 0;
         nextLast = 0;
+    }
+    //copy的size,nextFirst跟nextLast呢?
+    public ArrayDeque(ArrayDeque other){
+    //不考慮resize，都是固定大小8
+        items = new int[8];
+        for(int i=0;i<items.length;i++){
+            items[i] = other.items[i];
+        }
+        size = other.size;
+        nextFirst = other.nextFirst;
+        nextLast = other.nextLast;
     }
     private void resize(){
         int [] a = new int [(int)(size*3.75)];
@@ -24,6 +37,7 @@ public class ArrayDeque{
         nextLast = size;
         a = null;
     }
+
     public void addLast(int x){
         if(size == items.length){
             resize();
@@ -34,9 +48,12 @@ public class ArrayDeque{
             if(nextFirst<0)
                 nextFirst = nextFirst+items.length;
         }
+        if(nextLast >= items.length)
+            nextLast = nextLast - items.length;
         size++;
 
     }
+
     public void addFirst(int x){
         if(size == items.length){
             resize();
@@ -45,11 +62,59 @@ public class ArrayDeque{
         /**以防empty array的時候先addFirst(還沒考慮Remove後size = 0的情形)*/
         if(size==0){
             nextLast = nextFirst + 2;
-            if(nextFirst<0){
-                nextFirst = nextFirst + items.length;
-            }
+        }
+        if(nextFirst<0){
+            nextFirst = nextFirst + items.length;
         }
         size++;
 
+    }
+
+    public int removeFirst(){
+        int output;
+        int removedIndex = nextFirst + 1;
+        //改成size
+        if (removedIndex >= items.length) {
+            removedIndex -= items.length;
+        }
+        output = items[removedIndex];
+        items[removedIndex] = 0;
+        nextFirst= removedIndex;
+//        if(size==items.length)
+//            nextLast-=1;
+        size--;
+        return output;
+    }
+
+    public int removeLast(){
+        int output;
+        int removedIndex = nextLast - 1;
+        if (removedIndex < 0) {
+            removedIndex += items.length;
+        }
+        output = items[removedIndex];
+        items[removedIndex] = 0;
+        nextLast= removedIndex;
+//        if(size==items.length)
+//            nextLast-=1;
+        size--;
+        return output;
+    }
+
+    public boolean isEmpty(){
+        if(size==0)
+            return true;
+        return false;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        for(int i=0;i<items.length;i++)
+            System.out.print(items[i]+" ");
+        System.out.println();
+    }
+    public int get(int index){
+        return items[index];
     }
 }
--- Submit_repo/proj1a/ArrayDeque_own.java
+++ Submit_repo/proj1a/ArrayDeque_own.java
@@ -1,15 +1,48 @@
+/**往前可行*/
 public class ArrayDeque_own{
     public static void main(String[] args){
         ArrayDeque a = new ArrayDeque();
+//        System.out.println(a.size());
         a.addLast(0);
         a.addLast(1);
         a.addLast(2);
+//        System.out.println(a.size());
+        a.printDeque();
+
         a.addLast(3);
         a.addFirst(7);
-        a.addLast(4);
-        a.addLast(5);
         a.addFirst(6);
+        a.addFirst(5);
+        ArrayDeque a_copy = new ArrayDeque(a);
+        System.out.println(a.get(5));
+//        a.printDeque();
+
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+
+        a.addFirst(5);
+        a.addLast(4);
+        a.addFirst(100);
+
+        a.removeFirst();
+        a.addLast(6);
+        a.addFirst(7);
+        a.addFirst(1);
+        a.addFirst(2);
+        a.addFirst(3);
+
         a.addLast(8);
+        a.removeFirst();
+        a.removeFirst();
+         a.removeFirst();
+        a.addFirst(7);
+        a.addFirst(6);
+
+        a.addLast(7);
 
 
 
